0aa2f9073360d9a5b024de42381994dd3589f30d
Jashan Sambyal <sambyaljashan@gmail.com>
2026-07-04 18:22:38 +0530

    added if statement for os

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos_storage.txt"):
+    with open("todos_storage.txt", 'w') as file:
+        pass
+
 
 sg.theme('DarkGrey2')
 
